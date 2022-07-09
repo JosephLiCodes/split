fb68e0330ee7aeb85752a2b7b584de9abdb0b9ec
CollinQ <collinqian@ucsb.edu>
2022-07-08 20:04:58 -0700

    ios app initial commit

---
+++ split/Shared/splitApp.swift
@@ -0,0 +1,17 @@
+//
+//  splitApp.swift
+//  Shared
+//
+//  Created by Collin Qian on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct splitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
